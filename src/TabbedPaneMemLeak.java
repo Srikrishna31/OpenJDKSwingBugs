7518903c847bd1e61a2bf51f257edd5c6fe55f59
Krishna Addepalli <coolkrishna31@gmail.com>
2018-12-18 12:58:33 +0530

    Added reflection based check for TabbedPaneMemLeak.java for JDK-6714324
    Signed-off-by: Krishna Addepalli <coolkrishna31@gmail.com>

--- src/TabbedPaneMemLeak.java
+++ src/TabbedPaneMemLeak.java
@@ -28,13 +28,43 @@
  * to the Page (AccessibleContext) object.
  * @run main TabbedPaneMemLeak
  */
+import javax.accessibility.Accessible;
+import javax.accessibility.AccessibleContext;
 import javax.swing.JTabbedPane;
 import javax.swing.JComponent;
 import javax.swing.JPanel;
+import javax.swing.JSlider;
 import javax.swing.SwingUtilities;
+
+import java.awt.Component;
+import java.lang.reflect.Field;
+import java.util.Hashtable;
 
 public class TabbedPaneMemLeak
 {
+    private static void checkAccessibleParent(Component component) {
+        //Use reflection to check the value of accessibleContext, since directly calling getAccessibleContext()
+        //creates one, if not already present.
+        try {
+            Field field =
+                    component.getClass().getSuperclass().getSuperclass().getSuperclass().getSuperclass().getDeclaredField(
+                            "accessibleContext");
+            field.setAccessible(true);
+            AccessibleContext ctx = (AccessibleContext)field.get(component);
+            if (ctx != null) {
+                Field accessibleParentField = field.getType().getDeclaredField("accessibleParent");
+                accessibleParentField.setAccessible(true);
+                Accessible parent = (Accessible)accessibleParentField.get(ctx);
+                if (parent != null) {
+                    throw new RuntimeException("Test failed: AccessibleContext added on the wrong codepath.");
+                }
+            }
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            throw new RuntimeException("Test failed: Unable to fetch AccessibleContext");
+        }
+
+    }
+
     public static void main(String[] args) throws Exception
     {
         SwingUtilities.invokeAndWait(() -> {
@@ -44,12 +74,35 @@ public class TabbedPaneMemLeak
                 System.out.println(component.getAccessibleContext().getAccessibleParent()); // null
                 tabbedPane.addTab("Component", component);
                 System.out.println(component.getAccessibleContext().getAccessibleParent()); // JTabbedPane$Page
-                tabbedPane.removeAll(); // Could also be tabbedPane.remove(component) or tabbedPane.removeTabAt(0)
 
+                JComponent component1 = new JPanel();
+                JComponent component2 = new JPanel();
+
+                tabbedPane.addTab("Component1", component1);
+                tabbedPane.setComponentAt(1, component2);
+
+                if (component1.getAccessibleContext().getAccessibleParent() != null) {
+                    throw new RuntimeException("Test failed: Parent AccessibleContext not cleared from the child component");
+                }
+
+                tabbedPane.removeAll(); // Could also be tabbedPane.remove(component) or tabbedPane.removeTabAt(0)
                 if (component.getAccessibleContext().getAccessibleParent() != null) {
                     throw new RuntimeException("Test failed: Parent AccessibleContext not cleared from the child " +
-                            "compoent");
+                            "component");
                 }
+
+                JSlider slider = new JSlider(0, 10);
+                Hashtable<Integer, JComponent> labels = slider.createStandardLabels(5, 2);
+
+                JComponent labelComp = labels.get(labels.keys().nextElement());
+
+                tabbedPane.add(labelComp);
+
+                checkAccessibleParent(labelComp);
+
+                tabbedPane.remove(labelComp);
+
+                checkAccessibleParent(labelComp);
             }
         });
     }
