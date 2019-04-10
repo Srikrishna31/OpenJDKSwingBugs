bd5f5a0040d808e6f96bba8055761e41c1d1deb2
Krishna Addepalli <coolkrishna31@gmail.com>
2019-04-10 11:38:58 +0530

    Reproducing JDK-8222177: JAWS - DB Client Install Summary page tree contents not read in detail
    Signed-off-by: Krishna Addepalli <coolkrishna31@gmail.com>

---
+++ src/TreeAccessibilityExample.java
@@ -0,0 +1,43 @@
+/*
+JDK-8222177
+ */
+import javax.swing.JFrame;
+import javax.swing.JTree;
+import javax.swing.SwingUtilities;
+
+import javax.swing.tree.DefaultMutableTreeNode;
+
+public class TreeAccessibilityExample {
+    JFrame f;
+    TreeAccessibilityExample() {
+        showUI();
+    }
+
+    private void showUI() {
+        try {
+            SwingUtilities.invokeAndWait(() -> {
+                f=new JFrame();
+                DefaultMutableTreeNode style=new DefaultMutableTreeNode("Style");
+                DefaultMutableTreeNode color=new DefaultMutableTreeNode("color");
+                DefaultMutableTreeNode font=new DefaultMutableTreeNode("font");
+                style.add(color);
+                style.add(font);
+                DefaultMutableTreeNode red=new DefaultMutableTreeNode("red");
+                DefaultMutableTreeNode blue=new DefaultMutableTreeNode("blue");
+                DefaultMutableTreeNode black=new DefaultMutableTreeNode("black");
+                DefaultMutableTreeNode green=new DefaultMutableTreeNode("green");
+                color.add(red); color.add(blue); color.add(black); color.add(green);
+                JTree jt=new JTree(style);
+                f.add(jt);
+                f.setSize(200,200);
+                f.setVisible(true);
+            });
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void main(String[] args) {
+        new TreeAccessibilityExample();
+    }
+}
